2dd61219b5a0ab1d15aab10b606a9441e37d4717
romak maharjan <romakmaharjan@gmail.com>
2024-02-05 09:32:06 +0545

    first commit

---
+++ introduction/index.php
@@ -0,0 +1,18 @@
+<?php
+// echo "hello world";
+// echo "hello world again";
+
+// echo $name;
+// $name = "romak";
+$name="romak";
+echo $name;
+echo "<br>";
+$name="maharjan";
+echo $name;
+echo "<br>";
+$name="maharjan";
+$name="maharjan2"; //called or display only last name
+echo $name;
+
+
+?>
