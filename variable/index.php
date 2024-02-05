316a7899517355ea4d4d524852b25de26c4ab54f
romak maharjan <romakmaharjan@gmail.com>
2024-02-05 11:19:40 +0545

    add update

---
+++ variable/index.php
@@ -0,0 +1,28 @@
+<?php
+// what is variable?
+// variable is a container which store data.
+
+// rule for variable
+// 1. variable start with $ sign.
+// 2. variable name start with letter or underscore.
+// 3. variable name can't start with number.
+// 4. variable name can't contain special
+// character except underscore.
+
+// example of variable
+// $name='ram';
+
+// name$ = 'ramak'; // wrong
+// user name = 'ramak'; // wrong
+
+$name="romak";
+$x=777;
+
+echo $name;
+echo "<br>";
+echo $x;
+
+echo "<br>";
+$txt = "website.com";
+echo "I like $txt!";
+?>
