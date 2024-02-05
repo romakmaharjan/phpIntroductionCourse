a5f284c0470b2b13408acfdc9abb3ab16bc92b9c
romak maharjan <romakmaharjan@gmail.com>
2024-02-05 12:15:20 +0545

    add datatype

---
+++ datatype/index.php
@@ -0,0 +1,51 @@
+<?php
+// what is data type?
+// data type is a classification of 
+//data which tell the compiler or
+// interpreter how the programmer intends to use the data.
+
+// list of data type
+// 1. string
+// 2. integer
+// 3. float
+// 4. boolean
+// 5. array
+// 6. object
+// 7. resource
+// 8. null
+// 9. undefined
+
+
+// how to check data type?
+// we can check data type by using gettype() function.
+
+$name='romak';
+echo "my name is $name";
+
+// $a =2234.567;
+// echo gettype($a);
+
+// $is_pass = false;
+// var_dump($is_pass);
+// echo $is_pass;
+
+// $data =[1,2,3,4,5];
+
+// echo gettype($data);
+
+// class Person{
+//     public $name;
+//     public $address;
+// }
+
+// $p = new Person();
+
+// echo gettype($p);
+
+
+// $a =null;
+// echo gettype($a);
+
+// $name='';
+// echo gettype($name);
+?>
