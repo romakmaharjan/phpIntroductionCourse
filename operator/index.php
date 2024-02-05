de688972c3fe7032b35e7fd052a79b6453b870e1
romak maharjan <romakmaharjan@gmail.com>
2024-02-05 12:49:04 +0545

    add operators

---
+++ operator/index.php
@@ -0,0 +1,61 @@
+<?php
+// what is operator?
+// operator is a symbol that perform operation on one or
+// more operand.
+
+// list of operator
+// 1. arithmetic operator: +, -, *, /, %, **
+// example $x + $y $x - $y 
+
+// echo 4**5;
+
+// 2. assignment operator: =, +=, -=, *=, /=, %=, **=
+
+// $a = 5;
+// $a **= 2;
+// echo $a;
+// example x += y	same x = x + y
+
+// 3. comparison operator: ==, ===, !=, !==, >, <, >=, <=
+
+// $a = 5;
+// $b = '5';
+//example ==	Equal	$x == $y	Returns true if $x is equal to $y.
+//example ===	Identical	$x === $y	Returns true if $x is equal to $y, and they are of the same type.
+
+// if($a===$b){
+//     echo "both are same";
+// }else{
+//     echo "both are different";
+// }
+
+// 4. increment/decrement operator: ++$a, $a++, --$a, $a--
+//example ++$x	Pre-increment	Increments $x by one, then returns $x
+//example $x++	Post-increment	Returns $x, then increments $x by one
+
+// 5. logical operator: &&, ||, !
+//example (and)	And	$x and $y	True if both $x and $y are true
+//example (or)	Or	$x or $y	True if either $x or $y is true
+//example (&&)	And	$x && $y	True if both $x and $y are true
+//example (||)	Or	$x || $y	True if either $x or $y is true
+//example (!)	Not	!$x	True if $x is not true
+
+// 6. string operator: .
+
+
+// $name='ram';
+
+// echo $name.' sharma';
+//example (.)	Concatenation	$txt1 . $txt2	Concatenation of $txt1 and $txt2
+
+// 7. Array Assignmnet operator: +, ==, !=, <>, !==
+
+//example (==)	Equality	$x == $y	Returns true if $x and $y have the same key/value pairs
+
+// 8. Conditional Assignment operator: ?:, ??
+//example (?:)	Ternary	$x = expr1 ? expr2 : expr3	Returns the value of $x.
+// The value of $x is expr2 if expr1 = TRUE.
+// The value of $x is expr3 if expr1 = FALSE
+
+
+?>
