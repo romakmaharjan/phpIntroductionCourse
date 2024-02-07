8e474488843465edbccf7beebf68a0ed7b4e18fa
romak maharjan <romakmaharjan@gmail.com>
2024-02-07 13:36:47 +0545

    updates

--- databaseConnect/insert.php
+++ databaseConnect/insert.php
@@ -11,21 +11,18 @@ if($conn == false){
                 . mysqli_connect_error());
 }
 
- // Taking all 5 values from the form data(input)
+ // Taking all 2 values from the form data(input)
         
         $first_name =  $_REQUEST['first_name'];      
         $email = $_REQUEST['email'];
 
           // Performing insert query execution
         // here our table name is college
-        $sql = "INSERT INTO phpstudent  VALUES ('$id','$first_name','$email')";
+        $sql = "INSERT INTO phpstudent (first_name, email) VALUES ('$first_name', '$email')";
          
         if(mysqli_query($conn, $sql)){
-            echo "<h3>data stored in a database successfully."
-                . " Please browse your localhost php my admin"
-                . " to view the updated data</h3>"; 
- 
-            echo nl2br("\n$first_name\n  ". " $email");
+            echo "<h3>data stored in a database successfully.</h3>"; 
+            echo nl2br("\n First name:$first_name\n Email: $email");
         } else{
             echo "ERROR: Hush! Sorry $sql. "
                 . mysqli_error($conn);
